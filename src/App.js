ee1c3c0f4092abe3bd2074cef591d56ca72f27ce
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2017-12-21 19:50:29 -0500

    Create simple graph.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { LineChart } from 'react-easy-chart'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
+    const initialWidth = window.innerWidth > 0 ? window.innerWidth : 500
+    const initialHeight = window.innerHeight > 0 ? window.innerHeight : 500
     this.state = {
-      data: {}
+      data: [],
+      graphWidth: initialWidth,
+      graphHeight: initialHeight
     }
+
+    this.handleResize = this.handleResize.bind(this)
   }
+
+  handleResize() {
+    this.setState({
+      graphWidth: window.innerWidth > 0 ? window.innerWidth : 500,
+      graphHeight: window.innerHeight > 0 ? window.innerHeight : 500
+    })
+  }
+
   componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
     const docClient = new AWS.DynamoDB.DocumentClient() //eslint-disable-line no-undef
 
-    docClient.scan({
-      TableName: 'btx',
-      FilterExpression: 'attribute_exists(utc)'
-    }, (err, data) => {
-      if (err) console.error(err)
-      this.setState(() => ({
-        data: data
-      }))
-    })
+    docClient.scan(
+      {
+        TableName: 'btx',
+
+      },
+      (err, data) => {
+        if (err) console.error(err)
+        this.setState(() => ({
+          // TODO: change AWS dynamo data type to send a number.
+          data: [
+            data.Items.sort((a, b) => {
+              const dateA = Date.parse(a.date)
+              const dateB = Date.parse(b.date)
+                return dateA - dateB
+            }).map((item, index) => ({
+              x: Number(Date.parse(item.date)),
+              y: Number(item.value)
+            }))
+          ]
+        }))
+      }
+    )
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {JSON.stringify(this.state.data)}
-        </p>
+        <LineChart axes width={this.state.graphWidth} height={this.state.graphHeight} data={this.state.data} />
       </div>
     )
   }
