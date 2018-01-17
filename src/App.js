89118bf7659b347b3e9b6ec019f2d82cd8a605fa
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2018-01-17 14:09:57 +0000

    Change chart library, add debounce.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import { LineChart } from 'react-easy-chart'
+import { ScatterChart, Scatter, XAxis, YAxis } from 'recharts'
+import debounce from 'debounce'
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     const initialWidth = window.innerWidth > 0 ? window.innerWidth : 500
-    const initialHeight = window.innerHeight > 0 ? window.innerHeight : 500
+    const initialHeight = window.innerHeight > 0 ? window.innerHeight - 80 : 500
     this.state = {
       data: [],
       graphWidth: initialWidth,
@@ -16,36 +17,38 @@ class App extends Component {
     this.handleResize = this.handleResize.bind(this)
   }
 
+  debounceResize() {
+    return debounce(2000, this.handleResize())
+  }
+
   handleResize() {
-    this.setState({
+    return this.setState({
       graphWidth: window.innerWidth > 0 ? window.innerWidth : 500,
-      graphHeight: window.innerHeight > 0 ? window.innerHeight : 500
+      graphHeight: window.innerHeight > 0 ? window.innerHeight - 80 : 500
     })
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.handleResize)
+    window.addEventListener('resize', this.debounceResize.bind(this))
     const docClient = new AWS.DynamoDB.DocumentClient() //eslint-disable-line no-undef
-
     docClient.scan(
       {
-        TableName: 'btx',
-
+        TableName: 'btx'
       },
       (err, data) => {
         if (err) console.error(err)
         this.setState(() => ({
           // TODO: change AWS dynamo data type to send a number.
-          data: [
-            data.Items.sort((a, b) => {
+          data: data.Items
+            .sort((a, b) => {
               const dateA = Date.parse(a.date)
               const dateB = Date.parse(b.date)
-                return dateA - dateB
-            }).map((item, index) => ({
+              return dateA - dateB
+            })
+            .map((item, index) => ({
               x: Number(Date.parse(item.date)),
               y: Number(item.value)
             }))
-          ]
         }))
       }
     )
@@ -54,7 +57,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LineChart axes width={this.state.graphWidth} height={this.state.graphHeight} data={this.state.data} />
+        <h1> Current value of BTX wallet (in BTC) </h1>
+        <ScatterChart
+          isAnimationActive={false}
+          interpolate="cardinal"
+          margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
+          width={this.state.graphWidth}
+          height={this.state.graphHeight}
+        >
+          <XAxis dataKey={'x'} type="number" name="date" domain={['auto', 'auto']} />
+          <YAxis dataKey={'y'} type="number" name="btc value" />
+          <Scatter
+            shape={() => '.'}
+            isAnimationActive={false}
+            line
+            name="BTC value over time"
+            data={this.state.data}
+            fill="#0F75FF"
+          />
+        </ScatterChart>
       </div>
     )
   }
